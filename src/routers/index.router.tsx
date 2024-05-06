604a7f86145759220138f58212f2ea7078de9b6b
Duong-Van-Tu <17049351duongvantu@gmail.com>
2024-05-06 16:45:51 +0700

    fix route

--- src/routers/index.router.tsx
+++ src/routers/index.router.tsx
@@ -1,19 +1,101 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import NotfoundPage from '@/pages/notfound.page';
-import SaleRouter from '@/modules/sales/routers';
 import LoginPage from '@/pages/login.page';
-import SettingRouter from '@/modules/settings/routers';
-import PayrollRouter from '@/modules/payroll/routers';
-import ReportRouter from '@/modules/reports/routers';
+import Middleware from '@/middleware';
+import MainLayout from '@/layouts/main.layout';
+import KPIPage from '@/modules/sales/pages/kpi.page';
+import PrivilegesPage from '@/modules/sales/pages/privileges.page';
+import RelationshipPage from '@/modules/sales/pages/relationship.page';
+import OpportunityPage from '@/modules/sales/pages/opportunity.page';
+import SaleKitPage from '@/modules/sales/pages/sale-kit.page';
+import { SettingPage } from '@/modules/settings/pages';
+import PayrollPage from '@/modules/payroll/pages';
+import { ReportPage } from '@/modules/reports/pages';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="sales/*" element={<SaleRouter />} />
-        <Route path="settings/*" element={<SettingRouter />} />
-        <Route path="payroll/*" element={<PayrollRouter />} />
-        <Route path="reports/*" element={<ReportRouter />} />
+        <Route
+          path="sales/kpi"
+          element={
+            <Middleware mode="private">
+              <MainLayout>
+                <KPIPage />
+              </MainLayout>
+            </Middleware>
+          }
+        />
+        <Route
+          path="sales/privileges"
+          element={
+            <Middleware mode="private">
+              <MainLayout>
+                <PrivilegesPage />
+              </MainLayout>
+            </Middleware>
+          }
+        />
+        <Route
+          path="sales/relationship"
+          element={
+            <Middleware mode="private">
+              <MainLayout>
+                <RelationshipPage />
+              </MainLayout>
+            </Middleware>
+          }
+        />
+        <Route
+          path="sales/opportunity"
+          element={
+            <Middleware mode="private">
+              <MainLayout>
+                <OpportunityPage />
+              </MainLayout>
+            </Middleware>
+          }
+        />
+        <Route
+          path="sales/sale-kit"
+          element={
+            <Middleware mode="private">
+              <MainLayout>
+                <SaleKitPage />
+              </MainLayout>
+            </Middleware>
+          }
+        />
+        <Route
+          path="settings"
+          element={
+            <Middleware mode="private">
+              <MainLayout>
+                <SettingPage />
+              </MainLayout>
+            </Middleware>
+          }
+        />
+        <Route
+          path="payroll"
+          element={
+            <Middleware mode="private">
+              <MainLayout>
+                <PayrollPage />
+              </MainLayout>
+            </Middleware>
+          }
+        />
+        <Route
+          path="reports"
+          element={
+            <Middleware mode="private">
+              <MainLayout>
+                <ReportPage />
+              </MainLayout>
+            </Middleware>
+          }
+        />
         <Route path="login" element={<LoginPage />} />
         <Route path="not-found" element={<NotfoundPage />} />
         <Route path="*" element={<Navigate to="/not-found" replace />} />
