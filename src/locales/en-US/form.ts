d2f8d97b411ea3a8c23f07702c16389255d13ac3
Duong-Tu <vantuit99@gmail.com>
2024-05-10 23:05:40 +0700

    fix locale

--- src/locales/en-US/form.ts
+++ src/locales/en-US/form.ts
@@ -9,8 +9,8 @@ export const enUS_form = {
   'form.auth.rememberMe': 'Remember me',
   'form.input.require.username': 'Please input your username!',
   'form.input.require.password': 'Please input your password!',
-  'form.input.require.newPassword': 'Please password new!',
-  'form.input.require.newPasswordConfirm': 'Please password confirm new!',
+  'form.input.require.newPassword': 'Please enter a new password!',
+  'form.input.require.newPasswordConfirm': 'Please re-enter the new password!',
   'form.input.require.email': 'Please input your Email!',
   'form.input.require.otp': 'Please input your OTP!',
 };
