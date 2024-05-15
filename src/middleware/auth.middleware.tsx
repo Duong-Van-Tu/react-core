c6c55b8266701125f50b03b1c1fe7179f09467e9
Duong-Tu <vantuit99@gmail.com>
2024-05-15 22:26:58 +0700

    fix middleware

--- src/middleware/auth.middleware.tsx
+++ src/middleware/auth.middleware.tsx
@@ -1,7 +1,7 @@
 import PageIndicator from '@/components/page-indicator';
 import { useQuery } from '@/hooks/query.hook';
 import { useRootSelector } from '@/hooks/selector.hook';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 interface AuthProps {
   children: React.ReactElement;
@@ -11,6 +11,7 @@ interface AuthProps {
 export default function Auth({ children, logged }: AuthProps) {
   const { user } = useRootSelector((state) => state.auth);
   const { tenant } = useQuery();
+  const { pathname } = useLocation();
 
   if (!logged) {
     return <Navigate to={`/auth/login?tenant=${tenant}`} />;
@@ -18,6 +19,10 @@ export default function Auth({ children, logged }: AuthProps) {
 
   if (!user) {
     return <PageIndicator />;
+  }
+
+  if (pathname === '/') {
+    return <Navigate to={`/sales/kpi?tenant=${tenant}`} />;
   }
   return children;
 }
