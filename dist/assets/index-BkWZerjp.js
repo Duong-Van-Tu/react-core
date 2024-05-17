534eb5dabafa23eda58ce60834ede2ae748ccbde
Duong-Tu <vantuit99@gmail.com>
2024-05-17 20:39:55 +0700

    build

---
+++ dist/assets/index-BkWZerjp.js
@@ -0,0 +1,1 @@
+import{d as e,r as s,aK as a,o as r}from"./index-BW2778Vf.js";function c(){const t=e();return s.useEffect(()=>{t(a([{title:{vi_VN:"Setting",en_US:"Setting"}}]))},[t]),r("div",{children:"Setting page"})}export{c as default};
---
+++ dist/assets/notfound.page-BMAI1B70.js
@@ -0,0 +1,1 @@
+import{o}from"./index-BW2778Vf.js";const e=()=>o("div",{children:"NotfoundPage"});export{e as default};
