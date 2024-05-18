e0921780132b9060004d4ffe3a01a277e0de200d
Duong-Tu <vantuit99@gmail.com>
2024-05-18 20:00:52 +0700

    remove build

--- dist/assets/index-BX-elD6b.js
+++
@@ -1,2 +0,0 @@
-import{b as e,r,aN as t,o as s}from"./index-cELW97-M.js";function c(){const a=e();return r.useEffect(()=>{a(t([{title:{vi_VN:"Payroll",en_US:"Payroll"}}]))},[a]),s("div",{children:"Payroll page"})}export{c as default};
-//# sourceMappingURL=index-BX-elD6b.js.map
--- dist/assets/notfound.page-CKcfrO0t.js
+++
@@ -1,2 +0,0 @@
-import{o}from"./index-cELW97-M.js";const e=()=>o("div",{children:"NotfoundPage"});export{e as default};
-//# sourceMappingURL=notfound.page-CKcfrO0t.js.map
