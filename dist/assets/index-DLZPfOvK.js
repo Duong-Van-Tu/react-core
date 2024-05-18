0653f72d0b8a2c1655a66e79e50f46645b64c4cf
Duong-Tu <vantuit99@gmail.com>
2024-05-18 11:18:55 +0700

    build

---
+++ dist/assets/index-DLZPfOvK.js
@@ -0,0 +1,1 @@
+import{d as e,r as s,aK as a,o as r}from"./index-CzGvAkhO.js";function c(){const t=e();return s.useEffect(()=>{t(a([{title:{vi_VN:"Setting",en_US:"Setting"}}]))},[t]),r("div",{children:"Setting page"})}export{c as default};
---
+++ dist/assets/notfound.page-DjrOxlkR.js
@@ -0,0 +1,1 @@
+import{o}from"./index-CzGvAkhO.js";const e=()=>o("div",{children:"NotfoundPage"});export{e as default};
