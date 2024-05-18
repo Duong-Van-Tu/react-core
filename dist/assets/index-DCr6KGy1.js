e5f7213c5c7a615a0af97a9f7dec4663bd4b3aae
Duong-Tu <vantuit99@gmail.com>
2024-05-18 16:57:47 +0700

    build

---
+++ dist/assets/index-DCr6KGy1.js
@@ -0,0 +1,2 @@
+import{b as e,r,aM as t,o as s}from"./index-ohB5FR39.js";function c(){const a=e();return r.useEffect(()=>{a(t([{title:{vi_VN:"Payroll",en_US:"Payroll"}}]))},[a]),s("div",{children:"Payroll page"})}export{c as default};
+//# sourceMappingURL=index-DCr6KGy1.js.map
---
+++ dist/assets/message-_MIb7JSV.js
@@ -0,0 +1,7 @@
+import{e as t,r as f,o as u,bP as i}from"./index-ohB5FR39.js";function g(...r){const s=t(e=>{const n=e.loading.box;return r.map(o=>{const[a,c=void 0]=Array.isArray(o)?o:[o];return n[a]??c})});return f.useMemo(()=>s.filter(e=>e!=null),[s])}const x=({type:r,children:s,hasBackground:e})=>u("div",{css:d(r,e),children:s}),d=(r,s)=>i("font-size:1.6rem;font-weight:400;",s&&`background: ${r==="error"?"#ffe6e6":r==="success"?"#abf0ab":r==="warning"?"#fff7e6":"#fff0cc"};
+  padding: 4px;
+  display: flex;
+  justify-content: center;
+  border-radius: 4px;
+  `," color:",r==="error"?"#cc0000":r==="success"?"#299429":r==="warning"?"#F1CB46":"#ff9900",";","");export{x as M,g as u};
+//# sourceMappingURL=message-_MIb7JSV.js.map
