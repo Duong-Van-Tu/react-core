44ea90713d27872efccaccafc06ab4a96e69af66
Duong-Tu <vantuit99@gmail.com>
2024-05-18 16:56:33 +0700

    build

--- dist/assets/index-ZzdP4-D6.js
+++
@@ -1,2 +0,0 @@
-import{b as t,r,aM as s,o as a}from"./index-ohB5FR39.js";function p(){const e=t();return r.useEffect(()=>{e(s([{title:{vi_VN:"Report",en_US:"Report"}}]))},[e]),a("div",{children:"Report page"})}export{p as default};
-//# sourceMappingURL=index-ZzdP4-D6.js.map
