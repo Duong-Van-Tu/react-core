6f616082f974dde25ee02df034dce339cea7d8e7
Duong-Tu <vantuit99@gmail.com>
2024-05-25 12:50:15 +0700

    build

---
+++ dist/assets/loading.hook-B4xO64Zi.js
@@ -0,0 +1,2 @@
+import{b as s,r as c}from"./index-D7kYuqtd.js";function l(...e){const t=s(r=>{const a=r.loading.box;return e.map(o=>{const[n,u=void 0]=Array.isArray(o)?o:[o];return a[n]??u})});return c.useMemo(()=>t.filter(r=>r!=null),[t])}export{l as u};
+//# sourceMappingURL=loading.hook-B4xO64Zi.js.map
---
+++ dist/assets/notfound.page-57fDnAgn.js
@@ -0,0 +1,2 @@
+import{c as o}from"./index-D7kYuqtd.js";const e=()=>o("div",{children:"NotfoundPage"});export{e as default};
+//# sourceMappingURL=notfound.page-57fDnAgn.js.map
